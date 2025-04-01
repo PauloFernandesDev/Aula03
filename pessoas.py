c90c06b19e0e5e9d13a3dad1e7acc44216746708
Marcio Paulo <marciorocha11@gmail.com>
2025-04-01 14:02:55 -0300

    Aula03

---
+++ pessoas.py
@@ -0,0 +1,6 @@
+nome =  input("Informe seu nome: ")
+idade = input("Informe sua idade: ")
+salario = float (input("Informe seu salario: "))
+
+print(f"*********************************")
+print(f"**Nome: {nome} \n**Idade: {idade} \n**Salario: {salario}")
