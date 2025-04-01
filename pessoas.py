5146fe6a62b6ee76ba84064527944ca308ba8d4e
Marcio Paulo <marciorocha11@gmail.com>
2025-04-01 14:18:11 -0300

    Valor Salario atualizado

--- pessoas.py
+++ pessoas.py
@@ -3,4 +3,12 @@ idade = input("Informe sua idade: ")
 salario = float (input("Informe seu salario: "))
 
 print(f"*********************************")
+
 print(f"**Nome: {nome} \n**Idade: {idade} \n**Salario: {salario}")
+print(f"*********************************")
+percentual = float (input("** Me informe valor percentual de aumento (%): "))
+calculo = salario *(percentual / 100)
+calculo = calculo +salario
+print(f"**Nome: {nome} \n**Idade: {idade} \n**Salario Atualizado: {calculo}")
+print(f"*********************************")
+
