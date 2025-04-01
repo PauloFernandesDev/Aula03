681787f7dbd4ca76f99daf1e393e447555e489c9
Marcio Paulo <marciorocha11@gmail.com>
2025-04-01 15:37:23 -0300

    Maior ou menor numero

---
+++ Validação.py
@@ -0,0 +1,7 @@
+valora = float(input("Informe o valor A: "))
+valorb = float(input("Informe o valor B: "))
+
+if(valora > valorb):
+    print(f"Valor A ({valora}) é maior que valor B ({valorb})")
+else:
+    print(f"Valor B ({valorb}) é maior que valor A ({valora})")
