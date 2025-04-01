fe2d08eac24f90f9ee1fc9e9b84ae1931feb7559
Marcio Paulo <marciorocha11@gmail.com>
2025-04-01 16:14:50 -0300

    Status de aprovacao do aluno

--- verificar media e aprovacao.py
+++ verificar media e aprovacao.py
@@ -3,7 +3,17 @@ nota2 = float(input("Informe segunda nota: "))
 nota3 = float(input("Informe terceira nota: "))
 resultado  = (nota1 + nota2 + nota3)/ 3
 
-if resultado >= 7:
-    print(f"Media : {resultado} \nAluno Aprovado!!.")
+if resultado < 7:
+    if resultado >= 4:
+        print(f"Media : {resultado} \nAluno em recuperação!!.")
+    else:
+        print(f"Media : {resultado} \nAluno Reprovado!!.")
 else:
-    print(f"Media : {resultado} \nAluno Reprovado!.")
+    print(f"Media : {resultado} \nAluno em Aprovado!!.")
+
+
+
+
+
+
+
