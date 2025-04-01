7f36766cad430b0ffebd225a62edf3e72d735e1f
Marcio Paulo <marciorocha11@gmail.com>
2025-04-01 15:07:41 -0300

    Troca de numero constante

---
+++ Troca de lugar.py
@@ -0,0 +1,12 @@
+valora = 5
+valorb = 10
+
+print(f"Valor de A antes : {valora}")
+print(f"Valor de B antes : {valorb}")
+print("******************")
+valorc= valora
+valora = valorb
+print(f"Valor de A depois : {valora}")
+valorb = valorc
+print(f"Valor de B depois : {valorb}")
+
