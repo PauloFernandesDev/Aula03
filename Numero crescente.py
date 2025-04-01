4b6b788e2dd853d923272d5baaa504fd010a96a4
Marcio Paulo <marciorocha11@gmail.com>
2025-04-01 15:46:06 -0300

    numero em forma crescente

---
+++ Numero crescente.py
@@ -0,0 +1,7 @@
+numero1 = float(input("Informe o primeiro numero: "))
+numero2 = float(input("Informe o segundo numero: "))
+
+if(numero1 < numero2):
+    print(f"...{numero1}, {numero2}, ...")
+else:
+    print(f"...,{numero2},{numero1},...")
