f3410bd6f84e5c304e3babd6601cd1900c1909d6
Marcio Paulo <marciorocha11@gmail.com>
2025-04-03 14:29:08 -0300

    Alteracao no calculo combustivel

--- Calculo Conbustivel.py
+++ Calculo Conbustivel.py
@@ -3,13 +3,13 @@ g = 5.80
 
 
 
-combustivel = int(input("Informe o tipo de combustiver [1]- Gasolina / [2]- Etanol: "))
+combustivel = input("Informe o tipo de combustiver [G]- Gasolina / [E]- Etanol: ")
 litro = float(input("Informe quantidade de litros: "))
-if combustivel == 1:
+if combustivel == 'g' or 'G':
     calc = g * litro
     print(f"Valor total a pagar : ${calc:.2f}")
 else:
-    if combustivel == 2:
+    if combustivel == 'e' or 'E':
         calc = e * litro
         print(f"Valor total a pagar : ${calc:.2f}")
     else:
