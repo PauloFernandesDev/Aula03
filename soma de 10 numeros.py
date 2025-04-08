14568be0239b683dffe6ef94d32ed80e29e5d65d
Marcio Paulo <marciorocha11@gmail.com>
2025-04-08 16:58:50 -0300

    soma de 10 numero e taboada de multiplicacao

--- soma de 10 numeros.py
+++ soma de 10 numeros.py
@@ -1,6 +1,6 @@
-numero = 0
+soma = 0
 numeroin = 0
 for i in range(1,11,1):
     numeroin = int(input(f"Digite o {i}° numero: "))
-    numero = numero + numeroin
-print(f"A soma dos numeros digitados é: {numero}")
+    soma = soma + numeroin
+print(f"A soma dos numeros digitados é: {soma}")
---
+++ taboada de multiplicação.py
@@ -0,0 +1,6 @@
+numero = int(input("Digite um numero:"))
+resultado = 0
+for x in range(1,11,1):
+    resultado = numero * x
+    print(f"{numero} X {x} : {resultado}")
+
